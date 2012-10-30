56b527e70abdd8823265fbc27e19bb267b4b2932
Ian Hoover <polonian@gmail.com>
2012-10-29 20:50:48 -0400

    updated bitSlice

--- bitSlice_1bit.v
+++ bitSlice_1bit.v
@@ -1,6 +1,11 @@
-module bitSlice_1bit (Out, Cout, A, B,Cin,Cntrl)
+module bitSlice_1bit (Out, Cout, A, B,Cin,Cntrl);
   input A,B,Cin;
   input [2:0] Cntrl;
   output Out, Cout;
+  
+  wire Xor, Add, Sub, Slt, Mul;
+  
+  xor xorGate(Xor,A,B);
+  
   
 endmodule
---
+++ work/bit@slice_1bit/_primary.vhd
@@ -0,0 +1,12 @@
+library verilog;
+use verilog.vl_types.all;
+entity bitSlice_1bit is
+    port(
+        \Out\           : out    vl_logic;
+        Cout            : out    vl_logic;
+        A               : in     vl_logic;
+        B               : in     vl_logic;
+        Cin             : in     vl_logic;
+        Cntrl           : in     vl_logic_vector(2 downto 0)
+    );
+end bitSlice_1bit;
