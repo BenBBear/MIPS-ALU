4d422fdb903236403e70c206845fbaa535a39a58
Derek Redfern <redfern.derek@gmail.com>
2012-11-01 19:20:42 -0400

    Changed ALU

--- alu.v
+++ alu.v
@@ -8,8 +8,14 @@ module alu(inA,inB,Ctrl,zero,overflow,Cout)
   input [31:0] inA;
   input [31:0] inB;
   output zero,overflow,Cout;
+  wire [31:0] notB;
+  wire Cin;
+  
+  not_32bit notBgate(notB,inB);
+
   
   //32 instances of bitSlice
+  bitSlice_1bit bitSlice1(Out, Cout, A, B, Cin,Cntrl)
   
   //Handle overflows and check for zero
 endmodule
