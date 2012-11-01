70d7a7735c888e75a4e82c35a066419b5088859c
Derek Redfern <redfern.derek@gmail.com>
2012-11-01 19:55:56 -0400

    mux_5bit takes a control array; incremental changes to ALU

--- alu.v
+++ alu.v
@@ -4,18 +4,26 @@
 //Ian Hoover and Derek Redfern
 //Olin College - CompArch
 
-module alu(inA,inB,Ctrl,zero,overflow,Cout)
+`define NOR nor #320
+
+module alu(inA,inB,Out,Ctrl,zero,overflow,Cout)
   input [31:0] inA;
   input [31:0] inB;
+  input [2:0] Ctrl;
+  output [31:0] Out;
   output zero,overflow,Cout;
-  wire [31:0] notB;
-  wire Cin;
+  wire Cin,notCtrl1,notCtrl2;
   
-  not_32bit notBgate(notB,inB);
-
+  `NOT notCtrl1gate(notCtrl1,Ctrl[1]);
+  `NOT notCtrl2gate(notCtrl2,Ctrl[2]);
+  `AND Cingate(Cin,notCtrl2,notCtrl1,Ctrl[0]); //Cin is 1 for SUB (001).
   
   //32 instances of bitSlice
-  bitSlice_1bit bitSlice1(Out, Cout, A, B, Cin,Cntrl)
+  bitSlice_1bit bitSlice1(Out, Cout, A, B, Cin,Cntrl);
   
   //Handle overflows and check for zero
+  `NOR norZero(zero,Out[0],Out[1],Out[2],Out[3],Out[4],Out[5],Out[6],Out[7],
+                Out[8],Out[9],Out[10],Out[11],Out[12],Out[13],Out[14],Out[15],
+                Out[16],Out[17],Out[18],Out[19],Out[20],Out[21],Out[22],Out[23],
+                Out[24],Out[25],Out[26],Out[27],Out[28],Out[29],Out[30],Out[31])
 endmodule
--- mux_5bit.v
+++ mux_5bit.v
@@ -7,35 +7,37 @@
 `define NOT not #10
 
 //It might make more sense to take [2:0] s as an input. Change later?
-module mux_5bit(muxOut, in0, in1, in2, in3, in4, s0, s1, s2);
+module mux_5bit(muxOut, in0, in1, in2, in3, in4, s);
   output muxOut;
-  input in0, in1, in2, in3, in4, s0, s1, s2;
+  input in0, in1, in2, in3, in4;
+  input [2:0] s;
   wire andOut0, andOut1, andOut2, andOut3, andOut4, nots0, nots1, nots2;
   
-  `NOT nots0gate(nots0, s0);
-  `NOT nots1gate(nots1, s1);
-  `NOT nots2gate(nots2, s2);
-  `AND and0(andOut0,in0,nots0,nots1,nots2);
-  `AND and1(andOut1,in1,nots0,nots1,s2);
-  `AND and2(andOut2,in2,nots0,s1,nots2);
-  `AND and3(andOut3,in3,nots0,s1,s2);
-  `AND and4(andOut4,in4,s0,nots1,nots2);
+  `NOT nots0gate(nots0, s[0]);
+  `NOT nots1gate(nots1, s[1]);
+  `NOT nots2gate(nots2, s[2]);
+  `AND and0(andOut0,in0,nots2,nots1,nots0);
+  `AND and1(andOut1,in1,nots2,nots1,s[0]);
+  `AND and2(andOut2,in2,nots2,s[1],nots0);
+  `AND and3(andOut3,in3,nots2,s[1],s[0]);
+  `AND and4(andOut4,in4,s[2],nots1,nots0);
   `OR finalOr(muxOut,andOut0,andOut1,andOut2,andOut3,andOut4);
 endmodule
 
 module TESTmux_5bit;
-  reg A,B,C,D,E,s0,s1,s2; 
+  reg A,B,C,D,E; 
+  reg [2:0] s;
   wire muxOut; 
    
   //test mux
   initial  // Stimulus 
   begin 
-    A=0; B=1; C=0; D=1; E=1; s0=0; s1=1; s2=1; 
-    #150 s2=0; 
-    #150 s1=0;
-    #150 s0=1; 
+    A=0; B=1; C=0; D=1; E=1; s[2]=0; s[1]=1; s[0]=1; 
+    #150 s[0]=0; 
+    #150 s[1]=0;
+    #150 s[2]=1; 
   end 
-  mux_5bit UUT (muxOut,A,B,C,D,E,s0,s1,s2); 
+  mux_5bit UUT (muxOut,A,B,C,D,E,s); 
   initial  // Response 
-    $monitor($time,muxOut,A,B,C,D,E,s0,s1,s2);
+    $monitor($time,muxOut,A,B,C,D,E,s);
 endmodule
