b0e88b67dd6be7e0c360b5489b4c61864bf1bb26
Ian Hoover <polonian@gmail.com>
2012-11-04 20:12:12 -0500

    multiplication added to ALU code.  Accidentally to older file

--- alu.v
+++ alu.v
@@ -20,39 +20,78 @@ module alu(A,B,FinalOut,Ctrl,zero,overflow,CoutFinal);
   wire [31:0] Cout;
   wire [31:0] Out;
   
+  //multiplication overhead
+  wire [31:0] A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27, A28, A29, A30, A31;
+  wire [30:0] cInM0, cOutM0, cOutM1, cOutM2, cOutM3, cOutM4, cOutM5, cOutM6, cOutM7, cOutM8, cOutM9, cOutM10, cOutM11, cOutM12, cOutM13, cOutM14, cOutM15, cOutM16, cOutM17, cOutM18, cOutM19, cOutM20, cOutM21, cOutM22, cOutM23, cOutM24, cOutM25, cOutM26, cOutM27, cOutM28, cOutM29, cOutM30, cOutM31;
+
+  assign A0 = A<<31;
+  assign A1 = A<<30;
+  assign A2 = A<<29;
+  assign A3 = A<<28;
+  assign A4 = A<<27;
+  assign A5 = A<<26;
+  assign A6 = A<<25;
+  assign A7 = A<<24;
+  assign A8 = A<<23;
+  assign A9 = A<<22;
+  assign A10 = A<<21;
+  assign A11 = A<<20;
+  assign A12 = A<<19;
+  assign A13 = A<<18;
+  assign A14 = A<<17;
+  assign A15 = A<<16;
+  assign A16 = A<<15;
+  assign A17 = A<<14;
+  assign A18 = A<<13;
+  assign A19 = A<<12;
+  assign A20 = A<<11;
+  assign A21 = A<<10;
+  assign A22 = A<<9;
+  assign A23 = A<<8;
+  assign A24 = A<<7;
+  assign A25 = A<<6;
+  assign A26 = A<<5;
+  assign A27 = A<<4;
+  assign A28 = A<<3;
+  assign A29 = A<<2;
+  assign A30 = A<<1;
+  assign A31 = A;
+  assign cInM0 = 0;
+  
   //32 instances of bitSlice
-  bitSlice_1bit bitSlice0(Out[0], Cout[0], A[0], B[0], Ctrl[0],Ctrl); //Cin is 1 for SUB (instruction 001).
-  bitSlice_1bit bitSlice1(Out[1], Cout[1], A[1], B[1], Cout[0],Ctrl);
-  bitSlice_1bit bitSlice2(Out[2], Cout[2], A[2], B[2], Cout[1],Ctrl);
-  bitSlice_1bit bitSlice3(Out[3], Cout[3], A[3], B[3], Cout[2],Ctrl);
-  bitSlice_1bit bitSlice4(Out[4], Cout[4], A[4], B[4], Cout[3],Ctrl);
-  bitSlice_1bit bitSlice5(Out[5], Cout[5], A[5], B[5], Cout[4],Ctrl);
-  bitSlice_1bit bitSlice6(Out[6], Cout[6], A[6], B[6], Cout[5],Ctrl);
-  bitSlice_1bit bitSlice7(Out[7], Cout[7], A[7], B[7], Cout[6],Ctrl);
-  bitSlice_1bit bitSlice8(Out[8], Cout[8], A[8], B[8], Cout[7],Ctrl);
-  bitSlice_1bit bitSlice9(Out[9], Cout[9], A[9], B[9], Cout[8],Ctrl);
-  bitSlice_1bit bitSlice10(Out[10], Cout[10], A[10], B[10], Cout[9],Ctrl);
-  bitSlice_1bit bitSlice11(Out[11], Cout[11], A[11], B[11], Cout[10],Ctrl);
-  bitSlice_1bit bitSlice12(Out[12], Cout[12], A[12], B[12], Cout[11],Ctrl);
-  bitSlice_1bit bitSlice13(Out[13], Cout[13], A[13], B[13], Cout[12],Ctrl);
-  bitSlice_1bit bitSlice14(Out[14], Cout[14], A[14], B[14], Cout[13],Ctrl);
-  bitSlice_1bit bitSlice15(Out[15], Cout[15], A[15], B[15], Cout[14],Ctrl);
-  bitSlice_1bit bitSlice16(Out[16], Cout[16], A[16], B[16], Cout[15],Ctrl);
-  bitSlice_1bit bitSlice17(Out[17], Cout[17], A[17], B[17], Cout[16],Ctrl);
-  bitSlice_1bit bitSlice18(Out[18], Cout[18], A[18], B[18], Cout[17],Ctrl);
-  bitSlice_1bit bitSlice19(Out[19], Cout[19], A[19], B[19], Cout[18],Ctrl);
-  bitSlice_1bit bitSlice20(Out[20], Cout[20], A[20], B[20], Cout[19],Ctrl);
-  bitSlice_1bit bitSlice21(Out[21], Cout[21], A[21], B[21], Cout[20],Ctrl);
-  bitSlice_1bit bitSlice22(Out[22], Cout[22], A[22], B[22], Cout[21],Ctrl);
-  bitSlice_1bit bitSlice23(Out[23], Cout[23], A[23], B[23], Cout[22],Ctrl);
-  bitSlice_1bit bitSlice24(Out[24], Cout[24], A[24], B[24], Cout[23],Ctrl);
-  bitSlice_1bit bitSlice25(Out[25], Cout[25], A[25], B[25], Cout[24],Ctrl);
-  bitSlice_1bit bitSlice26(Out[26], Cout[26], A[26], B[26], Cout[25],Ctrl);
-  bitSlice_1bit bitSlice27(Out[27], Cout[27], A[27], B[27], Cout[26],Ctrl);
-  bitSlice_1bit bitSlice28(Out[28], Cout[28], A[28], B[28], Cout[27],Ctrl);
-  bitSlice_1bit bitSlice29(Out[29], Cout[29], A[29], B[29], Cout[28],Ctrl);
-  bitSlice_1bit bitSlice30(Out[30], Cout[30], A[30], B[30], Cout[29],Ctrl);
-  bitSlice_1bit bitSlice31(Out[31], Cout[31], A[31], B[31], Cout[30],Ctrl);
+  //Cin is 1 for SUB (instruction 001).
+  bitSlice_1bit bitSlice0(Out[0], Cout[0], cOutM0, A[0], B[0], A0, B, Ctrl[0], cIn0, Ctrl);
+  bitSlice_1bit bitSlice1(Out[1], Cout[1], cOutM1, A[1], B[1], A1, B, Cout[0], cIn1, Ctrl);
+  bitSlice_1bit bitSlice2(Out[2], Cout[2], cOutM2, A[2], B[2], A2, B, Cout[1], cIn2, Ctrl);
+  bitSlice_1bit bitSlice3(Out[3], Cout[3], cOutM3, A[3], B[3], A3, B, Cout[2], cIn3, Ctrl);
+  bitSlice_1bit bitSlice4(Out[4], Cout[4], cOutM4, A[4], B[4], A4, B, Cout[3], cIn4, Ctrl);
+  bitSlice_1bit bitSlice5(Out[5], Cout[5], cOutM5, A[5], B[5], A5, B, Cout[4], cIn5, Ctrl);
+  bitSlice_1bit bitSlice6(Out[6], Cout[6], cOutM6, A[6], B[6], A6, B, Cout[5], cIn6, Ctrl);
+  bitSlice_1bit bitSlice7(Out[7], Cout[7], cOutM7, A[7], B[7], A7, B, Cout[6], cIn7, Ctrl);
+  bitSlice_1bit bitSlice8(Out[8], Cout[8], cOutM8, A[8], B[8], A8, B, Cout[7], cIn8, Ctrl);
+  bitSlice_1bit bitSlice9(Out[9], Cout[9], cOutM9, A[9], B[9], A9, B, Cout[8], cIn9, Ctrl);
+  bitSlice_1bit bitSlice10(Out[10], Cout[10], cOutM10, A[10], B[10], A10, B, Cout[9], cIn10, Ctrl);
+  bitSlice_1bit bitSlice11(Out[11], Cout[11], cOutM11, A[11], B[11], A11, B, Cout[10], cIn11, Ctrl);
+  bitSlice_1bit bitSlice12(Out[12], Cout[12], cOutM12, A[12], B[12], A12, B, Cout[11], cIn12, Ctrl);
+  bitSlice_1bit bitSlice13(Out[13], Cout[13], cOutM13, A[13], B[13], A13, B, Cout[12], cIn13, Ctrl);
+  bitSlice_1bit bitSlice14(Out[14], Cout[14], cOutM14, A[14], B[14], A14, B, Cout[13], cIn14, Ctrl);
+  bitSlice_1bit bitSlice15(Out[15], Cout[15], cOutM15, A[15], B[15], A15, B, Cout[14], cIn15, Ctrl);
+  bitSlice_1bit bitSlice16(Out[16], Cout[16], cOutM16, A[16], B[16], A16, B, Cout[15], cIn16, Ctrl);
+  bitSlice_1bit bitSlice17(Out[17], Cout[17], cOutM17, A[17], B[17], A17, B, Cout[16], cIn17, Ctrl);
+  bitSlice_1bit bitSlice18(Out[18], Cout[18], cOutM18, A[18], B[18], A18, B, Cout[17], cIn18, Ctrl);
+  bitSlice_1bit bitSlice19(Out[19], Cout[19], cOutM19, A[19], B[19], A19, B, Cout[18], cIn19, Ctrl);
+  bitSlice_1bit bitSlice20(Out[20], Cout[20], cOutM20, A[20], B[20], A20, B, Cout[19], cIn20, Ctrl);
+  bitSlice_1bit bitSlice21(Out[21], Cout[21], cOutM21, A[21], B[21], A21, B, Cout[20], cIn21, Ctrl);
+  bitSlice_1bit bitSlice22(Out[22], Cout[22], cOutM22, A[22], B[22], A22, B, Cout[21], cIn22, Ctrl);
+  bitSlice_1bit bitSlice23(Out[23], Cout[23], cOutM23, A[23], B[23], A23, B, Cout[22], cIn23, Ctrl);
+  bitSlice_1bit bitSlice24(Out[24], Cout[24], cOutM24, A[24], B[24], A24, B, Cout[23], cIn24, Ctrl);
+  bitSlice_1bit bitSlice25(Out[25], Cout[25], cOutM25, A[25], B[25], A25, B, Cout[24], cIn25, Ctrl);
+  bitSlice_1bit bitSlice26(Out[26], Cout[26], cOutM26, A[26], B[26], A26, B, Cout[25], cIn26, Ctrl);
+  bitSlice_1bit bitSlice27(Out[27], Cout[27], cOutM27, A[27], B[27], A27, B, Cout[26], cIn27, Ctrl);
+  bitSlice_1bit bitSlice28(Out[28], Cout[28], cOutM28, A[28], B[28], A28, B, Cout[27], cIn28, Ctrl);
+  bitSlice_1bit bitSlice29(Out[29], Cout[29], cOutM29, A[29], B[29], A29, B, Cout[28], cIn29, Ctrl);
+  bitSlice_1bit bitSlice30(Out[30], Cout[30], cOutM30, A[30], B[30], A30, B, Cout[29], cIn30, Ctrl);
+  bitSlice_1bit bitSlice31(Out[31], Cout[31], cOutM31, A[31], B[31], A31, B, Cout[30], cIn31, Ctrl);
 
   `NOT notCtrl2gate(notCtrl2,Ctrl[2]);
   `AND3 sltFlagGate(sltFlag,notCtrl2,Ctrl[1],Ctrl[0]);  
@@ -112,12 +151,12 @@ module TESTalu;
   begin 
     A=2;
     B=5;
-    Ctrl[2]=0; Ctrl[1]=1; Ctrl[0]=1; 
+    Ctrl[2]=1; Ctrl[1]=0; Ctrl[0]=0; 
     //#150 Ctrl[0]=0; 
     //#150 Ctrl[1]=0;
     //#150 Ctrl[2]=1; 
   end 
   alu UUT (A,B,Out,Ctrl,zero,overflow,Cout);
   initial  // Response
-    $monitor($time,A,B,Out,Ctrl,zero,overflow,Cout);
+    $monitor($time,A,B,Out, ,Ctrl,zero,overflow,Cout);
 endmodule
